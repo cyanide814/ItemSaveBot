b57d27aa03da573b0cbf62a903591ac9c4437a89
cyanide814 <37595612+cyanide814@users.noreply.github.com>
2019-12-23 22:39:01 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,12 +2,12 @@ import praw
 import time
 import prawcore
 
-user = '' #Bot Username
-user_pass = '' #Bot Password
+user = 'GMKBot' #Bot Username
+user_pass = 'GMKBot1!' #Bot Password
 c_id = '' #Bot Client ID
 c_secret = '' #Bot Client Secret
 
-itemKeywords = ['Example', 'Example1'] #To add more keywords, just add more apostrophes separated by commas
+itemKeywords = ['Skeletor', 'GMK Skeletor'] #To add more keywords, just add more apostrophes separated by commas
 
 reddit = praw.Reddit(
     username = user,
@@ -18,13 +18,13 @@ reddit = praw.Reddit(
 )
 
 def main():
-    sub = reddit.subreddit('')  # Subreddit you want the bot to run in
+    sub = reddit.subreddit('mechmarket')  # Subreddit you want the bot to run in
 
     for submission in sub.stream.submissions():
         title = submission.title.lower() #Makes the title completely lowercase
         for phrase in itemKeywords:
             if phrase in title:
-                reddit.redditor('').message('An Item Has Been Posted!', 'A submission with a keyword you chose has been posted here: ' + submission) #Enter your username in the redditor box
+                reddit.redditor('cyanide814').message('An Item Has Been Posted!', 'A submission with a keyword you chose has been posted here: ' + submission) #Enter your username in the redditor box
 
 
 if __name__ == '__main__':
